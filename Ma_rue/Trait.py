b42d358ab93fc787e0361f1bedc6563526e30e34
Lenniegadet <lennie.gadet@eleves.ecmorlaix.fr>
2023-01-16 09:22:23 +0000

    MAJ

--- Ma_rue/Trait.py
+++ Ma_rue/Trait.py
@@ -12,5 +12,11 @@ def trait(x1,y1,x2,y2):
     rue.stroke()
     
     # Tests
- 
+if __name__ == '__main__' :
+    affiche(rue)
+    trait(50, 25, rue.width/2, rue.height/2)
+
+# Autres tests
+for x in range (int(rue.width/2), rue.width + 1, 20) :
+    trait(x, 0, 3*rue.width/2 - x, rue.height)
 
